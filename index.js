5d88aed0e2eed757103cad73042819cf67d0cda3
carrotiusha <ahsuitorrac>
2025-11-03 04:33:10 +0100

    using css flex/grid now; almost finished message box

--- index.js
+++ index.js
@@ -21,13 +21,37 @@ window.db_promise = initSqlJs({ locateFile: file => `/${file}` }).then(function(
 			window.feed_carrot(food)
 
 			var new_message = window.carrot_generate();
-			message.innerHTML = new_message;
+			response.innerHTML = new_message;
 		});
 		console.log("sql-js inited");
 	});
 })
 
 
+function send_message() {
+	var new_message = window.carrot_generate(message.value);
+	response.innerHTML = new_message;
+	if (!ignore_toggle.classList.contains('enabled')) {
+		window.feed_carrot(message.value);
+		message.value = "";
+	}
+}
+
+function toggle_ignore()
+{
+	if (!ignore_toggle.classList.contains('enabled')) {
+		ignore_toggle.classList.add("enabled");
+	}
+	else {
+		ignore_toggle.classList.remove("enabled");
+	}
+	if (ignore_toggle.classList.contains('enabled')) {
+		ignore_toggle.title = "Not ignore: carrot will eat your message; (I)"
+	}
+	else {
+		ignore_toggle.title = "Ignore: carrot will not eat your message; (I)"
+	}
+}
 
 window.onload = function () {
 	console.log("ONLOAD");
@@ -42,12 +66,25 @@ window.onload = function () {
 
 	refresh.onclick = e => {
 		var new_message = window.carrot_generate();
-		message.innerHTML = new_message;
+		response.innerHTML = new_message;
 	}
+
+	message.onkeydown = e => {
+		if(e.keyCode == 13){
+			send_message();
+		}
+	}
+
+	ignore_toggle.onclick = e => {
+		toggle_ignore();	
+	}
+
+	send.onclick = send_message;
+	toggle_ignore();	
 }
 document.addEventListener("DOMContentLoaded", function(event) {
 	console.log("DOMContentLoaded");
-	message.innerHTML = "...";
+	response.innerHTML = "...";
 });
 
 
@@ -57,8 +94,12 @@ window.onkeydown = e => {
         case "KeyR":
             if (e.ctrlKey === false) {
 				var new_message = window.carrot_generate();
-				message.innerHTML = new_message;
-			} break
+				response.innerHTML = new_message;
+			}
+			break
+		case "KeyI":
+			toggle_ignore();	
+			break
     }
 }
 
